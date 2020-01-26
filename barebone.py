726ccdc4d7124f3f4aadd0494c3167dac4ca6de6
James Benedict <jamesbenedict@Jamess-MacBook-Pro-2.local>
2020-01-26 09:39:26 -0500

    reaload working again"

--- barebone.py
+++ barebone.py
@@ -4,25 +4,28 @@ import os
 
 
 def compile_sass():
-    scss =  "style/style.scss"
-    css = "style/style.css"
+    cwd = os.getcwd()
+    scss_dir = os.path.join(cwd + "/style/sass/")
+    css_dir = os.path.join(cwd + "/style/css/")
+    scss = os.path.join(scss_dir + 'style.scss')
+    css = os.path.join(css_dir + 'style.css')
 
     if not os.path.exists(scss):
         with open(scss, 'a'): pass
-
     if not os.path.exists(css):
         with open(css, 'a'): pass
 
     try:
-        sass.compile(dirname=("style", "style"))
+        sass.compile(dirname=(scss_dir, css_dir))
         print('yoyoyo')
     except sass.CompileError:
+        print('compile error')
         pass
 
 
 def server():
     server = Server()
-    server.watch('style/style.scss', compile_sass)
+    server.watch('./style/sass/style.scss', compile_sass)
     server.serve(port=8080, host='localhost')
 
 
